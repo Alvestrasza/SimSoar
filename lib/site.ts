7d86a3c91b7e5f29b838d18f21ee85a404ec507b
ACS-Nouramon <nouramon@alvestrasza.com>
2026-05-31 13:21:00 +0200

    feat: add copyright year and repository URL to site configuration

--- lib/site.ts
+++ lib/site.ts
@@ -1,3 +1,6 @@
 export const SITE_VERSION = "0.2.0";
 export const SITE_COPYRIGHT_HOLDER1 = "SimSoar Project Team";
 export const SITE_COPYRIGHT_HOLDER2 = "Tim Richter";
+export const SITE_COPYRIGHT_YEAR = "2026";
+export const SITE_REPOSITORY_URL = "https://github.com/alvestrasza/simsoar";
+
