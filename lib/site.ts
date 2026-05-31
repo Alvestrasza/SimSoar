b79bd8a544936e415687e1721e4be4af1fb5df91
ACS-Nouramon <nouramon@alvestrasza.com>
2026-05-31 13:07:31 +0200

    feat: add bug report template for issue tracking

--- lib/site.ts
+++ lib/site.ts
@@ -1,2 +1,3 @@
 export const SITE_VERSION = "0.2.0";
-export const SITE_COPYRIGHT_HOLDER = "Tim Richter";
+export const SITE_COPYRIGHT_HOLDER1 = "SimSoar Project Team";
+export const SITE_COPYRIGHT_HOLDER2 = "Tim Richter";
